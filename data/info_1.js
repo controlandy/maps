306db65ad4e6e7ea499a8b6f002934782d45d921
root <root@idm.opennova.pe>
2020-04-11 18:30:06 -0500

    first commit

---
+++ data/info_1.js
@@ -0,0 +1,11 @@
+var json_info_1 = {
+"type": "FeatureCollection",
+"name": "info_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nombre": "Casa de Andy" }, "geometry": { "type": "Point", "coordinates": [ -77.118213513765454, -11.997754985406454 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nombre": "Inglesia de Andy" }, "geometry": { "type": "Point", "coordinates": [ -77.117986636356861, -11.997968532368857 ] } },
+{ "type": "Feature", "properties": { "id": "3", "nombre": "Galletas de Andy" }, "geometry": { "type": "Point", "coordinates": [ -77.118198531295064, -11.998316069220538 ] } },
+{ "type": "Feature", "properties": { "id": "4", "nombre": "Colegio de Andy" }, "geometry": { "type": "Point", "coordinates": [ -77.118005899533074, -11.998550551662541 ] } }
+]
+}
